5cab32f46463a4fb9576ba5766126e8697134cda
hooni <jhlee909090@gmail.com>
2024-06-26 15:19:18 +0900

    setting: 스토리보드 삭제했다 수연아 이제 운동가자
    - 스토리보드 삭제
    - SceneDelegate 기본화면 Viewcontroller로 연결
    - ViewController 노란색으로 연결확인

--- KkuMulKum/SceneDelegate.swift
+++ KkuMulKum/SceneDelegate.swift
@@ -13,11 +13,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
+           
+           guard let windowScene = (scene as? UIWindowScene) else { return }
+           self.window = UIWindow(windowScene: windowScene)
+           let navigationController = UINavigationController(rootViewController: ViewController())
+           self.window?.rootViewController = navigationController
+           self.window?.makeKeyAndVisible()
+       }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
