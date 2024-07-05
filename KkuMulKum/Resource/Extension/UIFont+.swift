916873f9242209fc1330670f7dc6b62765e4df37
mmaybei <yebbi2@ewhain.net>
2024-07-06 02:38:03 +0900

    setting/#123 텍스트 스타일 추가

--- KkuMulKum/Resource/Extension/UIFont+.swift
+++ KkuMulKum/Resource/Extension/UIFont+.swift
@@ -13,27 +13,31 @@ extension UIFont {
     }
     
     enum Pretendard {
-        case title01, title02
+        case title00, title01, title02
         case head01, head02
         case body01, body02, body03, body04, body05, body06
         case caption01, caption02
-        case label01, label02
+        case label00, label01, label02
         
         var weight: String {
             switch self {
-            case .title01, .head01, .body01, .body03, .body05, .caption01, .label01: "Pretendard-SemiBold"
-            case .title02, .head02, .body02, .body04, .body06, .caption02, .label02: "Pretendard-Regular"
+            case .title00: 
+                "Pretendard-Bold"
+            case .title01, .head01, .body01, .body03, .body05, .caption01, .label01:
+                "Pretendard-SemiBold"
+            case .title02, .head02, .body02, .body04, .body06, .caption02, .label00, .label02:
+                "Pretendard-Regular"
             }
         }
         
         var size: CGFloat {
             switch self {
-            case .title01, .title02: 24
+            case .title00, .title01, .title02: 24
             case .head01, .head02: 22
             case .body01, .body02: 18
             case .body03, .body04: 16
             case .body05, .body06: 14
-            case .caption01, .caption02: 12
+            case .caption01, .caption02, .label00: 12
             case .label01, .label02: 10
             }
         }
