8a46440c88ed0357593a5112cb7c9b6dcffc7975
hooni <jhlee909090@gmail.com>
2024-07-08 15:18:35 +0900

    Revert "feat: 카카오 로그인 구현"
    This reverts commit b1ba43214ef91529b1d0534a781e0c75b0789ede.

--- KkuMulKum/Application/AppDelegate.swift
+++ KkuMulKum/Application/AppDelegate.swift
@@ -13,39 +13,23 @@ import KakaoSDKAuth
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-   
-   
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        // KakaoSDK 초기화 과정에서 앱 키를 동적으로 불러오기
-        if let kakaoAppKey = fetchKakaoAppKeyFromPrivacyInfo() {
-            KakaoSDK.initSDK(appKey: kakaoAppKey)
-            print("Kakao SDK initialized with app key: \(kakaoAppKey)")
-        } else {
-            print("Failed to load KAKAO_APP_KEY from PrivacyInfo.plist")
-        }
+        KakaoSDK.initSDK(appKey: "${NATIVE_APP_KEY}")
 
         return true
     }
-    
-    func fetchKakaoAppKeyFromPrivacyInfo() -> String? {
-        guard let path = Bundle.main.path(forResource: "PrivacyInfo", ofType: "plist"),
-              let dict = NSDictionary(contentsOfFile: path) as? [String: AnyObject],
-              let appKey = dict["NATIVE_APP_KEY"] as? String else {
-            return nil
-        }
-        return appKey
-    }
 
-    // 카카오 로그인
+    //카카오 로그인
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        if AuthApi.isKakaoTalkLoginUrl(url) {
-            return AuthController.handleOpenUrl(url: url)
-        }
-        return false
-    }
+           if (AuthApi.isKakaoTalkLoginUrl(url)) {
+               return AuthController.handleOpenUrl(url: url)
+           }
+
+           return false
+       }
     
     // MARK: UISceneSession Lifecycle
 
