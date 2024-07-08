e2fad6aa2172c62facc7d80143f2ca7c1b97b4e6
mmaybei <yebbi2@ewhain.net>
2024-07-08 19:47:43 +0900

    feat/#137 Meetings 네트워크 모델 구현

---
+++ KkuMulKum/Network/DTO/Model/Meetings/MembersOfMeetingResponseModel.swift
@@ -0,0 +1,24 @@
+//
+//  MembersOfMeetingResponseModel.swift
+//  KkuMulKum
+//
+//  Created by 예삐 on 7/8/24.
+//
+
+import Foundation
+
+struct MembersOfMeetingResponseModel: Codable {
+    let memberCount: Int
+    let members: [Member]
+}
+
+struct Member: Codable {
+    let id: Int
+    let name, profileImageURLString: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case profileImageURLString = "profileImg"
+    }
+}
