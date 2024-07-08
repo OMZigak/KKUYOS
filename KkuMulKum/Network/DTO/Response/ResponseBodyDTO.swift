d4ce589adbeadf63be3385ef9e1c3355cd58a4ed
JinUng41 <ingjwjw@naver.com>
2024-07-08 16:14:24 +0900

    feat/#135 제네릭 ResponseBodyDTO 구현 및 EmptyModel 구현

---
+++ KkuMulKum/Network/DTO/Response/ResponseBodyDTO.swift
@@ -0,0 +1,20 @@
+//
+//  ResponseBodyDTO.swift
+//  KkuMulKum
+//
+//  Created by 김진웅 on 7/8/24.
+//
+
+import Foundation
+
+/// 제네릭 ResponseBody 구조체 정의
+struct ResponseBodyDTO<T: Codable>: Codable {
+    let data: T?
+    let error: ErrorResponse?
+}
+
+/// 에러 응답 구조체 정의
+struct ErrorResponse: Codable {
+    let code: Int
+    let message: String
+}
