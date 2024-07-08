7ca83fa21a9ace791d83bc965caf72f44bf011b0
JinUng41 <ingjwjw@naver.com>
2024-07-08 19:31:02 +0900

    feat/#138 로그인한 유저 정보 조회 API ResponseModel 구현

---
+++ KkuMulKum/Network/DTO/Model/Users/LoginUserResponseModel.swift
@@ -0,0 +1,26 @@
+//
+//  LoginUserResponseModel.swift
+//  KkuMulKum
+//
+//  Created by 김진웅 on 7/8/24.
+//
+
+import Foundation
+
+struct LoginUserResponseModel: Codable {
+    let name: String
+    let level: Int
+    let promiseCount: Int
+    let tardyCount: Int
+    let tardySum: Int
+    let profileImageURLString: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case level
+        case promiseCount
+        case tardyCount
+        case tardySum
+        case profileImageURLString = "profileImg"
+    }
+}
