2ff89a9a9069757e0a06bfe556153bb6c6eb0be4
hooni <jhlee909090@gmail.com>
2024-07-10 00:27:53 +0900

    fix/#148 충돌 해결

---
+++ KkuMulKum/Source/Onboarding/Login/ViewController/LoginViewController.swift
@@ -0,0 +1,62 @@
+//
+//  LoginVC.swift
+//  KkuMulKum
+//
+//  Created by 이지훈 on 7/9/24.
+//
+
+import UIKit
+
+import AuthenticationServices
+
+class LoginViewController: BaseViewController {
+    
+    private let loginView = LoginView()
+    private let loginViewModel = LoginViewModel()
+    
+    override func loadView() {
+        view = loginView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bindViewModel()
+    }
+    
+    override func setupAction() {
+        super.setupAction()
+        
+        let appleTapGesture = UITapGestureRecognizer(target: self, action: #selector(appleLoginTapped))
+        loginView.appleLoginImageView.addGestureRecognizer(appleTapGesture)
+        
+        let kakaoTapGesture = UITapGestureRecognizer(target: self, action: #selector(kakaoLoginTapped))
+        loginView.kakaoLoginImageView.addGestureRecognizer(kakaoTapGesture)
+    }
+    
+    private func bindViewModel() {
+        loginViewModel.loginState.bind(with: self) { owner, state in
+            switch state {
+            case .notLoggedIn:
+                print("Not logged in")
+            case .loggedIn(let userInfo):
+                print("Logged in: \(userInfo)")
+            }
+        }
+        
+        loginViewModel.error.bind(with: self) { owner, error in
+            if !error.isEmpty {
+                // TODO: 추후 에러처리 추가예정 -> Keychain 연결 이후
+                print("Error occurred: \(error)")
+            }
+        }
+    }
+
+    
+    @objc private func appleLoginTapped() {
+        loginViewModel.performAppleLogin(presentationAnchor: view.window!)
+    }
+    
+    @objc private func kakaoLoginTapped() {
+        loginViewModel.performKakaoLogin(presentationAnchor: view.window!)
+    }
+}
