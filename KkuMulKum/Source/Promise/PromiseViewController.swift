f3405fb7d643b0943b87b3e696f1a8009d157a6f
youz2me <kynhun20@gachon.ac.kr>
2024-07-09 22:08:15 +0900

    feat/#146 PromiseViewModel 생성

--- KkuMulKum/Source/Promise/PromiseViewController.swift
+++ KkuMulKum/Source/Promise/PromiseViewController.swift
@@ -8,35 +8,63 @@
 import UIKit
 
 class PromiseViewController: BaseViewController {
+    private let promiseViewModel = PromiseViewModel()
     
-    private let promiseView = PromiseView()
-
+    private lazy var promiseSegmentedControl = PromiseSegmentedControl(items: ["약속 정보",
+                                                                          "준비 현황",
+                                                                          "지각 꾸물이"])
+    
+    private let promisePageViewController = UIPageViewController(transitionStyle: .scroll,
+                                                                 navigationOrientation: .horizontal)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func setupView() {
         view.backgroundColor = .white
-        
-        // TODO: 서버 연결할 때 약속 이름으로 바인딩 필요
-        
         self.navigationItem.title = "기말고사 모각작"
         
-        view.addSubview(promiseView)
+        view.addSubview(promiseSegmentedControl)
+        addChild(promisePageViewController)
         
-        promiseView.snp.makeConstraints {
-            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        promiseSegmentedControl.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(-6)
+            $0.height.equalTo(52)
         }
+        
+        promisePageViewController.view.snp.makeConstraints {
+            $0.top.equalTo(promiseSegmentedControl.snp.bottom)
+            $0.horizontalEdges.bottom.equalToSuperview()
+        }
+        
+        promisePageViewController.didMove(toParent: self)
     }
     
     override func setupAction() {
-        promiseView.promiseSegmentedControl.addTarget(self, action: #selector(didSegmentedControlIndexUpdated), for: .valueChanged)
+        promiseSegmentedControl.addTarget(self, action: #selector(didSegmentedControlIndexUpdated), for: .valueChanged)
     }
     
+    override func setupDelegate() {
+        promisePageViewController.delegate = self
+        promisePageViewController.dataSource = self
+    }
     
     @objc private func didSegmentedControlIndexUpdated() {
-        promiseView.promiseSegmentedControl.selectedUnderLineView.snp.updateConstraints {
-            $0.leading.equalToSuperview().offset((promiseView.promiseSegmentedControl.bounds.width / CGFloat(promiseView.promiseSegmentedControl.numberOfSegments)) * CGFloat(promiseView.promiseSegmentedControl.selectedSegmentIndex))
+        promiseSegmentedControl.selectedUnderLineView.snp.updateConstraints {
+            $0.leading.equalToSuperview().offset((promiseSegmentedControl.bounds.width / CGFloat(promiseSegmentedControl.numberOfSegments)) * CGFloat(promiseSegmentedControl.selectedSegmentIndex))
         }
+    }
+}
+
+
+extension BaseViewController: UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return UIViewController()
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return UIViewController()
     }
 }
