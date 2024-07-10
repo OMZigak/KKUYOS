8636c805df5e0d2fd5a04284f640556b9266ea7f
hooni <jhlee909090@gmail.com>
2024-07-10 22:13:24 +0900

    setting/#154- 갤러리 권한접근 추기

--- KkuMulKum/Source/Onboarding/Profile/VIewController/ProfileViewController.swift
+++ KkuMulKum/Source/Onboarding/Profile/VIewController/ProfileViewController.swift
@@ -4,17 +4,32 @@
 //
 //  Created by 이지훈 on 7/10/24.
 //
+
 import UIKit
 
-class ProfileSetupViewController: BaseViewController {
+class ProfileSetupViewController: BaseViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     private let rootView = ProfileSetupView()
+    private let viewModel = ProfileSetupViewModel()
     
     override func loadView() {
         view = rootView
     }
     
-    override func setupView() {
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupBindings()
+        setupAction()
+    }
+    
+    private func setupBindings() {
+        viewModel.profileImage.bind(with: self) { (vc, image) in
+            vc.rootView.profileImageView.image = image
+        }
+        
+        viewModel.isConfirmButtonEnabled.bind(with: self) { (vc, isEnabled) in
+            vc.rootView.confirmButton.isEnabled = isEnabled
+            vc.rootView.confirmButton.alpha = isEnabled ? 1.0 : 0.5
+        }
     }
     
     override func setupAction() {
@@ -23,20 +38,37 @@ class ProfileSetupViewController: BaseViewController {
         rootView.cameraButton.addTarget(self, action: #selector(cameraButtonTapped), for: .touchUpInside)
     }
     
-    override func setupDelegate() {
-
-    }
-    
     @objc private func confirmButtonTapped() {
-
+        // TODO: 확인 버튼 탭 시 동작 구현
+        print("프로필 이미지 설정 완료")
     }
     
     @objc private func skipButtonTapped() {
-
+        // TODO: 건너뛰기 버튼 탭 시 동작 구현
+        print("프로필 이미지 설정 건너뛰기")
     }
     
     @objc private func cameraButtonTapped() {
-
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true)
+    }
+    
+    // MARK: - UIImagePickerControllerDelegate
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let editedImage = info[.editedImage] as? UIImage {
+            viewModel.updateProfileImage(editedImage)
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            viewModel.updateProfileImage(originalImage)
+        }
+        dismiss(animated: true)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true)
     }
     
 }
--- KkuMulKum/Source/Onboarding/Profile/ViewModel/ProfileViewModel.swift
+++ KkuMulKum/Source/Onboarding/Profile/ViewModel/ProfileViewModel.swift
@@ -5,17 +5,15 @@
 //  Created by 이지훈 on 7/10/24.
 //
 
-import Foundation
 
-class ProfileViewModel {
+import UIKit
+
+class ProfileSetupViewModel {
+    let profileImage = ObservablePattern<UIImage?>(UIImage.imgProfile)
+    let isConfirmButtonEnabled = ObservablePattern<Bool>(false)
     
-    var nickname: String = ""
-    
-    func setNickname(_ nickname: String) {
-        self.nickname = nickname
-    }
-    
-    func getNickname() -> String {
-        return nickname
+    func updateProfileImage(_ image: UIImage?) {
+        profileImage.value = image
+        isConfirmButtonEnabled.value = image != nil
     }
 }
