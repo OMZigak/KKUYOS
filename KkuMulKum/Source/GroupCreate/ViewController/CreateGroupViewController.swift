780d684a7ab1516feeb38575370df6d022a93fa8
youz2me <kynhun20@gachon.ac.kr>
2024-07-12 18:23:25 +0900

    feat/#157 그룹 생성 및 생성 완료 VC 구현

--- KkuMulKum/Source/GroupCreate/ViewController/CreateGroupViewController.swift
+++ KkuMulKum/Source/GroupCreate/ViewController/CreateGroupViewController.swift
@@ -8,16 +8,14 @@
 import UIKit
 
 class CreateGroupViewController: BaseViewController {
+    private let createGroupView: CreateGroupView = CreateGroupView()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func loadView() {
+        view = createGroupView
     }
-    */
-
+    
+    override func setupView() {
+        setupNavigationBarTitle(with: "내 모임 추가하기")
+        setupNavigationBarBackButton()
+    }
 }
