35efe8ede0f1fc3c0783e61cb726001d3d4a8e7e
mmaybei <yebbi2@ewhain.net>
2024-07-17 04:15:33 +0900

    fix/#185 서비스타입 ResponseBodyDTO로 바꿨어요 ...

--- KkuMulKum/Resource/Service/HomeService.swift
+++ KkuMulKum/Resource/Service/HomeService.swift
@@ -8,82 +8,94 @@
 import Foundation
 
 protocol HomeServiceType {
-    func fetchLoginUser() -> LoginUserModel?
-    func fetchNearestPromise() -> NearestPromiseModel?
-    func fetchUpcomingPromise() -> UpcomingPromiseListModel?
+    func fetchLoginUser() -> ResponseBodyDTO<LoginUserModel>
+    func fetchNearestPromise() -> ResponseBodyDTO<NearestPromiseModel>
+    func fetchUpcomingPromise() -> ResponseBodyDTO<UpcomingPromiseListModel>
 }
 
 final class MockHomeService: HomeServiceType {
-    func fetchLoginUser() -> LoginUserModel? {
-        let mockData = LoginUserModel(
-            name: "꾸물리안",
-            level: 4,
-            promiseCount: 8,
-            tardyCount: 2,
-            tardySum: 72,
-            profileImageURL: ""
+    func fetchLoginUser() -> ResponseBodyDTO<LoginUserModel> {
+        let mockData = ResponseBodyDTO<LoginUserModel>(
+            success: true,
+            data: LoginUserModel(
+                name: "꾸물리안",
+                level: 4,
+                promiseCount: 8,
+                tardyCount: 2,
+                tardySum: 72,
+                profileImageURL: ""
+            ),
+            error: nil
         )
         return mockData
     }
     
-    func fetchNearestPromise() -> NearestPromiseModel? {
-        let mockData = NearestPromiseModel(
-            id: 1,
-            dDay: 0,
-            name: "꾸물이들 대환장 파티",
-            meetingName: "꾸물이들",
-            dressUpLevel: "냐미",
-            date: "2024.07.16",
-            time: "PM 8:00",
-            placeName: "DMC역"
+    func fetchNearestPromise() -> ResponseBodyDTO<NearestPromiseModel> {
+        let mockData = ResponseBodyDTO<NearestPromiseModel>(
+            success: true,
+            data: NearestPromiseModel(
+                id: 1,
+                dDay: 0,
+                name: "꾸물이들 대환장 파티",
+                meetingName: "꾸물이들",
+                dressUpLevel: "냐미",
+                date: "2024.07.16",
+                time: "PM 8:00",
+                placeName: "DMC역"
+            ),
+            error: nil
         )
         return mockData
     }
     
-    func fetchUpcomingPromise() -> UpcomingPromiseListModel? {
-        let mockData = UpcomingPromiseListModel(
-            promises: [
-                UpcomingPromise(
-                    id: 1,
-                    dDay: 1,
-                    name: "누가 코코볼 다 먹었어?",
-                    meetingName: "우마우스",
-                    dressUpLevel: "",
-                    date: "2024.07.17",
-                    time: "PM 2:00",
-                    placeName: "가자하우스"
-                ),
-                UpcomingPromise(
-                    id: 2,
-                    dDay: 1,
-                    name: "누가 코코볼 다 먹었어?",
-                    meetingName: "우마우스",
-                    dressUpLevel: "",
-                    date: "2024.07.17",
-                    time: "PM 2:00",
-                    placeName: "가자하우스"
-                ),
-                UpcomingPromise(
-                    id: 3,
-                    dDay: 1,
-                    name: "누가 코코볼 다 먹었어?",
-                    meetingName: "우마우스",
-                    dressUpLevel: "",
-                    date: "2024.07.17",
-                    time: "PM 2:00",
-                    placeName: "가자하우스"
-                ),
-                UpcomingPromise(
-                    id: 4,
-                    dDay: 1,
-                    name: "누가 코코볼 다 먹었어?",
-                    meetingName: "우마우스",
-                    dressUpLevel: "",
-                    date: "2024.07.17",
-                    time: "PM 2:00",
-                    placeName: "가자하우스"
-                )
-            ]
+    func fetchUpcomingPromise() -> ResponseBodyDTO<UpcomingPromiseListModel> {
+        let mockData = ResponseBodyDTO<UpcomingPromiseListModel>(
+            success: true,
+            data: UpcomingPromiseListModel(
+                promises: [
+                    UpcomingPromise(
+                        id: 1,
+                        dDay: 1,
+                        name: "누가 코코볼 다 먹었어?",
+                        meetingName: "우마우스",
+                        dressUpLevel: "",
+                        date: "2024.07.17",
+                        time: "PM 2:00",
+                        placeName: "가자하우스"
+                    ),
+                    UpcomingPromise(
+                        id: 2,
+                        dDay: 1,
+                        name: "누가 코코볼 다 먹었어?",
+                        meetingName: "우마우스",
+                        dressUpLevel: "",
+                        date: "2024.07.17",
+                        time: "PM 2:00",
+                        placeName: "가자하우스"
+                    ),
+                    UpcomingPromise(
+                        id: 3,
+                        dDay: 1,
+                        name: "누가 코코볼 다 먹었어?",
+                        meetingName: "우마우스",
+                        dressUpLevel: "",
+                        date: "2024.07.17",
+                        time: "PM 2:00",
+                        placeName: "가자하우스"
+                    ),
+                    UpcomingPromise(
+                        id: 4,
+                        dDay: 1,
+                        name: "누가 코코볼 다 먹었어?",
+                        meetingName: "우마우스",
+                        dressUpLevel: "",
+                        date: "2024.07.17",
+                        time: "PM 2:00",
+                        placeName: "가자하우스"
+                    )
+                ]
+            ),
+            error: nil
         )
         return mockData
     }
