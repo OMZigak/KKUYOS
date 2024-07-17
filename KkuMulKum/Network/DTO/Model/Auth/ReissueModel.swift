b6436e5d2b8144a26732dd4ce3eaf81e56bf1acd
JinUng41 <ingjwjw@naver.com>
2024-07-17 21:09:48 +0900

    refactor/#200 Auth 모델 수정

--- KkuMulKum/Network/DTO/Model/Auth/ReissueModel.swift
+++ KkuMulKum/Network/DTO/Model/Auth/ReissueModel.swift
@@ -1,5 +1,5 @@
 //
-//  ResissueResponseModel.swift
+//  ReissueModel.swift
 //  KkuMulKum
 //
 //  Created by 이지훈 on 7/8/24.
@@ -7,6 +7,7 @@
 
 import Foundation
 
+/// 토큰 재발급 (Response)
 struct ReissueModel: ResponseModelType {
-    let accessToken, refreshToken: String?
+    let accessToken, refreshToken: String
 }
