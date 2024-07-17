4157750a8bbe9f1efde6da29d5aa662009f6dc98
youz2me <kynhun20@gachon.ac.kr>
2024-07-17 22:18:20 +0900

    feat/#204 PromiseTargetType 구현

--- KkuMulKum/Network/DTO/TargetType/MeetingTargetType.swift
+++ KkuMulKum/Network/DTO/TargetType/MeetingTargetType.swift
@@ -9,11 +9,15 @@ import Foundation
 
 import Moya
 
-enum PromiseTargetType {
-    case todayNext
+enum MeetingTargetType {
+    case createMeeting(request: MakeMeetingsRequestModel)
+    case joinMeeting(request: RegisterMeetingsModel)
+    case fetchMeetingList
+    case fetchMeetingInfo(meetingID: Int)
+    case fetchMeetingMember(meetingID: Int)
 }
 
-extension PromiseTargetType: TargetType {
+extension MeetingTargetType: TargetType {
     var baseURL: URL {
         guard let baseURL = URL(string: "/api/v1/meetings") else {
             fatalError("Error: Invalid Meeting BaseURL")
@@ -23,16 +27,36 @@ extension PromiseTargetType: TargetType {
     
     var path: String {
         switch self {
+        case .createMeeting:
+            return ""
+        case .joinMeeting:
+            return "/register"
+        case .fetchMeetingList:
+            return ""
+        case  .fetchMeetingInfo(meetingID: let meetingID):
+            return "/\(meetingID)"
+        case .fetchMeetingMember(meetingID: let meetingID):
+            return "/\(meetingID)/members"
         }
     }
     
     var method: Moya.Method {
         switch self {
+        case .createMeeting, .joinMeeting:
+            return .post
+        case .fetchMeetingList, .fetchMeetingInfo, .fetchMeetingMember:
+            return .get
         }
     }
     
     var task: Task {
         switch self {
+        case .createMeeting(request: let request):
+            return .requestJSONEncodable(request)
+        case .joinMeeting(request: let request):
+            return .requestJSONEncodable(request)
+        case .fetchMeetingList, .fetchMeetingInfo, .fetchMeetingMember:
+            return .requestPlain
         }
     }
     
@@ -47,4 +71,3 @@ extension PromiseTargetType: TargetType {
         ]
     }
 }
-
