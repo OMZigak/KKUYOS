8f547e195af4cd1173d2dc93a3bd2540c819491e
youz2me <kynhun20@gachon.ac.kr>
2024-07-19 00:42:17 +0900

    fix/#223 리뷰 반영

--- KkuMulKum/Source/Home/ViewController/HomeViewController.swift
+++ KkuMulKum/Source/Home/ViewController/HomeViewController.swift
@@ -126,18 +126,16 @@ extension HomeViewController: UICollectionViewDelegateFlowLayout {
         _ collectionView: UICollectionView,
         didSelectItemAt indexPath: IndexPath
     ) {
-        // TODO: promiseID를 모임 상세로 전달
-        print(
-            "promiseID: ",
-            viewModel.upcomingPromiseList.value?.data?.promises[indexPath.item].promiseID ?? 0
+        let pagePromiseViewController = PagePromiseViewController(
+            promiseViewModel: PagePromiseViewModel(
+                promiseID: viewModel.upcomingPromiseList.value?.data?.promises[indexPath.item].promiseID ?? 0
+            )
         )
-//        let viewController = PromiseInfoViewController(
-//            viewModel: PromiseInfoViewModel(
-//                promiseID: viewModel.upcomingPromiseList.value?.data?.promises[indexPath.item].promiseID ?? 0,
-//                service: PromiseService()
-//            )
-//        )
-        //tabBarController?.navigationController?.pushViewController(viewController, animated: true)
+        
+        tabBarController?.navigationController?.pushViewController(
+            pagePromiseViewController,
+            animated: true
+        )
     }
 }
 
