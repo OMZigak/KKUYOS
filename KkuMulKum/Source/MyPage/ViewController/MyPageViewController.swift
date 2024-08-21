39d4263ff5fec6fdefa40da6e431d1863ae9dc8b
hooni <jhlee909090@gmail.com>
2024-08-21 21:17:47 +0900

    feat/#319 배경색 수정

--- KkuMulKum/Source/MyPage/ViewController/MyPageViewController.swift
+++ KkuMulKum/Source/MyPage/ViewController/MyPageViewController.swift
@@ -22,6 +22,8 @@ class MyPageViewController: BaseViewController, CustomActionSheetDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .green1
+
         bindViewModel()
     }
     
