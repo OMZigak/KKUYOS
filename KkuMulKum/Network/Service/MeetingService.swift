097f8155f2fc9cc508921eb169fe21f4b7fe13e6
mmaybei <yebbi2@ewhain.net>
2024-08-26 20:22:05 +0900

    refactor/#336 미팅리스트 서비스 구현

--- KkuMulKum/Network/Service/MeetingService.swift
+++ KkuMulKum/Network/Service/MeetingService.swift
@@ -42,12 +42,6 @@ final class MeetingService {
     }
 }
 
-extension MeetingService: MeetingListServiceProtocol {
-    func fetchMeetingList() async throws -> ResponseBodyDTO<MeetingListModel>? {
-        return try await request(with: .fetchMeetingList)
-    }
-}
-
 extension MeetingService: MeetingInfoServiceProtocol {
     func fetchMeetingInfo(with meetingID: Int) async throws -> ResponseBodyDTO<MeetingInfoModel>? {
         return try await request(with: .fetchMeetingInfo(meetingID: meetingID))
@@ -85,27 +79,6 @@ extension MeetingService: InviteCodeServiceProtocol {
         with request: RegisterMeetingsModel
     ) async throws -> ResponseBodyDTO<RegisterMeetingsResponseModel>? {
         return try await self.request(with: .joinMeeting(request: request))
-    }
-}
-
-final class MockMeetingListService: MeetingListServiceProtocol {
-    func fetchMeetingList() async throws -> ResponseBodyDTO<MeetingListModel>? {
-        let mockData = ResponseBodyDTO<MeetingListModel>(
-            success: true,
-            data: MeetingListModel(
-                count: 6,
-                meetings: [
-                    Meeting(meetingID: 1, name: "꾸물이들", memberCount: 14),
-                    Meeting(meetingID: 2, name: "아요레디", memberCount: 28),
-                    Meeting(meetingID: 3, name: "안드가자", memberCount: 26),
-                    Meeting(meetingID: 4, name: "난이서버", memberCount: 30),
-                    Meeting(meetingID: 5, name: "캔디팟", memberCount: 24),
-                    Meeting(meetingID: 6, name: "열기팟", memberCount: 24)
-                ]
-            ),
-            error: nil
-        )
-        return mockData
     }
 }
 
--- KkuMulKum/Source/MeetingList/ServiceProtocol/MeetingListServiceProtocol.swift
+++ KkuMulKum/Source/MeetingList/ServiceProtocol/MeetingListServiceProtocol.swift
@@ -10,5 +10,6 @@ import Foundation
 import Moya
 
 protocol MeetingListServiceProtocol {
+    func fetchLoginUser() async throws -> ResponseBodyDTO<LoginUserModel>?
     func fetchMeetingList() async throws -> ResponseBodyDTO<MeetingListModel>?
 }
