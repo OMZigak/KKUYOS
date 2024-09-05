ebd51039cf9e671caf3b83fb00c3087c17ffcf0c
hooni <jhlee909090@gmail.com>
2024-09-05 21:43:54 +0900

    fix/#363 이용약관 탭바이슈 해결

--- KkuMulKum/Source/MyPage/ViewController/MyPageTermsViewController.swift
+++ KkuMulKum/Source/MyPage/ViewController/MyPageTermsViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 import WebKit
 
+import SnapKit
+
 class MyPageTermsViewController: BaseViewController {
     
     private let viewModel: MyPageViewModel
@@ -26,21 +28,46 @@ class MyPageTermsViewController: BaseViewController {
         let webConfiguration = WKWebViewConfiguration()
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
         webView.uiDelegate = self
-        view = webView
+        view = UIView()
+        view.addSubview(webView)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let myURL = URL(string: "https://arrow-frog-4b9.notion.site/a66033a3ff4a40bfaa6eff0a5bee737d")
-        let myRequest = URLRequest(url: myURL!)
-        webView.load(myRequest)
-    }
-    
-    override func setupView() {
-        super.setupView()
         setupNavigationBarTitle(with: "이용약관")
         setupNavigationBarBackButton()
+        
+        setupConstraints()
+        
+        if let myURL = URL(string: "https://arrow-frog-4b9.notion.site/a66033a3ff4a40bfaa6eff0a5bee737d") {
+            let myRequest = URLRequest(url: myURL)
+            webView.load(myRequest)
+        }
+    }
+    
+    private func setupConstraints() {
+        webView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        adjustWebViewContentInset()
+    }
+    
+    private func adjustWebViewContentInset() {
+        let contentInset = UIEdgeInsets(
+            top: 0,
+            left: 0,
+            bottom: view.safeAreaInsets.bottom + (tabBarController?.tabBar.frame.height ?? 0),
+            right: 0
+        )
+        webView.scrollView.contentInset = contentInset
+        webView.scrollView.scrollIndicatorInsets = contentInset
     }
 }
 
