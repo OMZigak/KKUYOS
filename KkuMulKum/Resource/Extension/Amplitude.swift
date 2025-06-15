2b24974b31f65216f5fb8ee1bfb65ab66a84c5f2
hooni <jhlee909090@gmail.com>
2025-06-15 19:39:21 +0900

    feat: #414- 앰플리튜드 기본 연결 확인

--- KkuMulKum/Resource/Extension/Amplitude.swift
+++ KkuMulKum/Resource/Extension/Amplitude.swift
@@ -5,4 +5,27 @@
 //  Created by 이지훈 on 6/15/25.
 //
 
-import Foundation
+import UIKit
+
+import Amplitude
+
+extension UIViewController {
+    
+    private static var previousScreen: String?
+    
+    func trackScreenView() {
+        let screenName = String(describing: type(of: self))
+        
+        var properties: [String: Any] = [
+            "screen_name": screenName,
+            "timestamp": Date().timeIntervalSince1970
+        ]
+        
+        if let prevScreen = UIViewController.previousScreen {
+            properties["previous_screen"] = prevScreen
+        }
+        
+        Amplitude.instance().logEvent("screen_viewed", withEventProperties: properties)
+        UIViewController.previousScreen = screenName
+    }
+}
